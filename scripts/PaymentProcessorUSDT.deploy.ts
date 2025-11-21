c5c5d5db7bc3fba761b9dc3a0ef771d128a3ee5c
Katerina Nikolaieva <k.nikolaieva@rocken.com>
2025-11-21 18:19:58 +0100

    init

--- scripts/PaymentProcessorUSDT.deploy.ts
+++ scripts/PaymentProcessorUSDT.deploy.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
-import { Address, Cell, toNano } from '@ton/core';
+import { Address, Cell, beginCell, toNano } from '@ton/core';
+import { TonClient, TupleBuilder } from '@ton/ton';
 import { type NetworkProvider } from '@ton/blueprint';
 import { JettonPaymentProcessor } from '../build/transfer-usdt/PaymentProcessorUSDT_JettonPaymentProcessor';
 
@@ -15,21 +16,14 @@ export async function run(provider: NetworkProvider) {
   const owner = Address.parse(requireEnv('OWNER'));
   const mainWallet = Address.parse(requireEnv('MAIN_COMMISSION_WALLET'));
   const nftCollection = Address.parse(requireEnv('NFT_COLLECTION'));
-
-  // master-адрес USDT (тот, что ты дал с tonviewer)
   const jettonMaster = Address.parse(requireEnv('USDT'));
-
-  // код jetton-кошелька в виде base64 BOC
-  const jettonWalletCodeHex = requireEnv('USDT_WALLET_CODE_HEX');
-  const jettonWalletCode = Cell.fromBoc(Buffer.from(jettonWalletCodeHex, 'hex'))[0];
 
   const contract = provider.open(
     await JettonPaymentProcessor.fromInit(
       owner,
       mainWallet,
       nftCollection,
-      jettonMaster,
-      jettonWalletCode,
+      jettonMaster
     ),
   );
 
@@ -42,4 +36,33 @@ export async function run(provider: NetworkProvider) {
   });
 
   console.log('PaymentProcessorUSDT deployed at', contract.address.toString());
+  // 3) Считаем jetton-кошелёк ЭТОГО контракта
+  //    ВАЖНО: тут используем адрес только что задеплоенного processor’а
+  const endpoint = 'https://testnet.toncenter.com/api/v2/jsonRPC'; // или свой
+  const client = new TonClient({ endpoint });
+
+  const tb = new TupleBuilder();
+  tb.writeAddress(contract.address);
+
+  const res = await client.runMethod(jettonMaster, 'get_wallet_address', tb.build());
+  const processorJettonWallet = res.stack.readAddress();
+
+  console.log('Processor jetton wallet:', processorJettonWallet.toString());
+
+  // 4) Отправляем от OWNER сообщение SetJettonWallet{ newWallet = processorJettonWallet }
+  const opSetJettonWallet = 0xccd3ca15; // тот же, что в контракте
+
+  const body: Cell = beginCell()
+    .storeUint(opSetJettonWallet, 32)           // можно number, можно BigInt
+    .storeAddress(processorJettonWallet)       // Address из TonClient.runMethod
+    .endCell();
+
+  await provider.sender().send({
+    to: contract.address,
+    value: toNano('0.05'), // газ на конфиг
+    bounce: true,
+    body,
+  });
+
+  console.log('SetJettonWallet sent');
 }
